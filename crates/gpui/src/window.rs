f3634e236a30600f11674804e1110de3779d7dff
Yara <git@yara.blue>
2026-05-28 11:32:12 +0200

    resolve actions eagerly

--- crates/gpui/src/window.rs
+++ crates/gpui/src/window.rs
@@ -4852,7 +4852,7 @@ impl Window {
             .remove(&action.as_any().type_id())
         {
             for listener in &global_listeners {
-                profiler::update_running_action(action);
+                profiler::update_running_action(action, cx);
                 listener(action.as_any(), DispatchPhase::Capture, cx);
                 profiler::save_action_timing();
                 if !cx.propagate_event {
@@ -4884,7 +4884,7 @@ impl Window {
             {
                 let any_action = action.as_any();
                 if action_type == any_action.type_id() {
-                    profiler::update_running_action(action);
+                    profiler::update_running_action(action, cx);
                     listener(any_action, DispatchPhase::Capture, self, cx);
                     profiler::save_action_timing();
 
@@ -4906,7 +4906,7 @@ impl Window {
                 let any_action = action.as_any();
                 if action_type == any_action.type_id() {
                     cx.propagate_event = false; // Actions stop propagation by default during the bubble phase
-                    profiler::update_running_action(action);
+                    profiler::update_running_action(action, cx);
                     listener(any_action, DispatchPhase::Bubble, self, cx);
                     profiler::save_action_timing();
 
@@ -4925,7 +4925,7 @@ impl Window {
             for listener in global_listeners.iter().rev() {
                 cx.propagate_event = false; // Actions stop propagation by default during the bubble phase
 
-                profiler::update_running_action(action);
+                profiler::update_running_action(action, cx);
                 listener(action.as_any(), DispatchPhase::Bubble, cx);
                 profiler::save_action_timing();
                 if !cx.propagate_event {
